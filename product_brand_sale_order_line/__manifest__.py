cc90a7d8e2eda003af3cd98794377245fce7f8ae
victor4pro <victor4pro@gmail.com>
2023-09-05 11:41:59 -0300

    [ADD] odoo_apps: product_brand_sale_order_line

---
+++ product_brand_sale_order_line/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Product Brand Sale Order Line",
+
+    'summary': """Product brand in sale order line""",
+
+    'author': "victor4pro",
+    'website': "https://github.com/victor4pro",
+
+    'category': 'sale',
+    'version': '0.1',
+
+    'depends': [
+        'product_brand',
+    ],
+
+    'data': [
+        'views/sale_order.xml',
+    ],
+
+    'demo': [
+    ],
+
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'GPL-3',
+}
